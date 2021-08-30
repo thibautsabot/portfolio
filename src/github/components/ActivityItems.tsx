b3c8f773e1eca2320052a447812d3da2b6ec2c7e
Thibaut SABOT <ginjiban@protonmail.ch>
2021-08-30 16:26:17 +0200

    add activity images

--- src/github/components/ActivityItems.tsx
+++ src/github/components/ActivityItems.tsx
@@ -1,12 +1,22 @@
 import Link from "next/link";
+import Image from "next/image";
+import fork from "../assets/fork.png";
+import unlock from "../assets/unlock.png";
+import star from "../assets/star.png";
+import branch from "../assets/branch.png";
+import release from "../assets/release.png";
+import styles from "./activity.module.css";
 
 function ForkItem({ event }) {
   return (
-    <Link passHref href={event.payload.forkee.html_url}>
-      <p>
-        New fork of &quot;{event.repo.name}&quot; -{" "}
-        {event.payload.forkee.description}
-      </p>
+    <Link href={event.payload.forkee.html_url}>
+      <a className={styles.itemLink}>
+        <Image width={24} height={24} alt="fork" src={fork} />
+        <p className={styles.itemText}>
+          New fork of <code>{event.repo.name}</code> -
+          &quot;{event.payload.forkee.description}&quot;
+        </p>
+      </a>
     </Link>
   );
 }
@@ -14,7 +24,12 @@ function ForkItem({ event }) {
 function PublicItem({ event }) {
   return (
     <Link passHref href={"https://github.com/" + event.repo.name}>
-      <p>&quot;{event.repo.name}&quot; was just made public !</p>
+      <a className={styles.itemLink}>
+        <Image width={24} height={24} alt="unlock" src={unlock} />
+        <p className={styles.itemText}>
+          <code>{event.repo.name}</code> was just made public!
+        </p>
+      </a>
     </Link>
   );
 }
@@ -22,24 +37,33 @@ function PublicItem({ event }) {
 function WatchItem({ event }) {
   return (
     <Link passHref href={"https://github.com/" + event.repo.name}>
-      <p>I starred the &quot;{event.repo.name}&quot; repository.</p>
+      <a className={styles.itemLink}>
+        <Image width={24} height={24} alt="star" src={star} />
+        <p className={styles.itemText}>
+          {" "}
+          <code>{event.repo.name}</code> is a new starred repository!
+        </p>
+      </a>
     </Link>
   );
 }
 
 function CreateItem({ event }) {
   if (event.payload.ref_type === "branch") {
-    if (event.payload.ref.indexOf("cms/blog") !== -1) return null;
+    // if (event.payload.ref.indexOf("cms/blog") !== -1) return null;
 
     return (
       <Link
         passHref
         href={`https://github.com/${event.repo.name}/tree/${event.payload.ref}`}
       >
-        <p>
-          The &quot;{event.payload.ref}&quot; branch was created on &quot;
-          {event.repo.name}&quot;.
-        </p>
+        <a className={styles.itemLink}>
+          <Image width={24} height={24} alt="branch" src={branch} />
+          <p className={styles.itemText}>
+            The <code>{event.payload.ref}</code> branch was created on{" "}
+            <code>{event.repo.name}</code>.
+          </p>
+        </a>
       </Link>
     );
   } else {
@@ -50,10 +74,13 @@ function CreateItem({ event }) {
 function ReleaseItem({ event }) {
   return (
     <Link passHref href={event.payload.release.html_url}>
-      <p>
-        New release &quot;{event.payload.release.body}&quot; on &quot;
-        {event.repo.name}&quot; !
-      </p>
+      <a className={styles.itemLink}>
+        <Image width={24} height={24} alt="release" src={release} />
+        <p className={styles.itemText}>
+          New release <code>{event.payload.release.body}</code> on{" "}
+          <code>{event.repo.name}</code>!
+        </p>
+      </a>
     </Link>
   );
 }
