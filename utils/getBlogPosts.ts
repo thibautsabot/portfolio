0f7021b74f8e7e300571212e200042c4ecac3a27
Thibaut Sabot <thibaut.sabot@adevinta.com>
2021-10-22 00:03:38 +0200

    use @next/mdx instead of mdx-bundler

--- utils/getBlogPosts.ts
+++ utils/getBlogPosts.ts
@@ -1,10 +1,19 @@
 import fs from "fs";
 import { join } from "path";
 
-export default function getBlogPosts({ limit = 10 }: { limit?: number }): string[] {
-    const blogPosts = fs.readdirSync(join(process.cwd(), "posts"));
+export default function getBlogPosts({
+  limit = 10,
+}: {
+  limit?: number;
+}): string[] {
+  const blogPosts = fs.readdirSync(join(process.cwd(), "pages", "blog"));
 
-    // TODO: Sort by last date
-    blogPosts.splice(limit, blogPosts.length)
-    return blogPosts.map((post) => post.replace(/.mdx/, ''))
+  // TODO: Sort by last date
+  const curatedBlocPosts = blogPosts
+    .filter((post) => post !== "index.tsx")
+    .map((post) => post.replace(/.mdx/, ""))
+
+  curatedBlocPosts.splice(limit, curatedBlocPosts.length)
+
+  return curatedBlocPosts
 }
