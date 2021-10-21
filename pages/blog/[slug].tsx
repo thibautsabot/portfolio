0a680dcaa983dafd90ef1a7e58d951af9ce391fc
Thibaut Sabot <thibaut.sabot@adevinta.com>
2021-10-21 23:28:21 +0200

    use mdx-bundler to format blog posts

--- pages/blog/[slug].tsx
+++ pages/blog/[slug].tsx
@@ -1,21 +1,28 @@
+import getBlogPostContent, { BlogContent } from "../../utils/getBlogPostContent";
+
 import { ReactElement } from "react";
-import getBlogPostContent from "../../utils/getBlogPostContent";
 import getBlogPosts from "../../utils/getBlogPosts";
+import { getMDXComponent } from 'mdx-bundler/client';
+import { useMemo } from 'react';
 
 interface Props {
-  content: string;
+  content: BlogContent;
 }
 
 export default function BlogPost({ content }: Props): ReactElement {
-  return <div>Hello {content}</div>;
+  const Component = useMemo(
+    () => getMDXComponent(content.code),
+    [content.code]
+  );
+  return <div>Hello <Component /></div>;
 }
 
 export async function getStaticProps({
   params,
 }: {
   params: { slug: string };
-}): Promise<{ props: { content: string } }> {
-  const content = getBlogPostContent(params.slug);
+}): Promise<{ props: { content: BlogContent } }> {
+  const content = await getBlogPostContent(params.slug);
 
   return {
     props: {
