0d0f390574e96e8e8c334ac757985140f28be593
Thibaut Sabot <thibaut.sabot@adevinta.com>
2021-10-28 14:37:25 +0200

    fix basic style for blog post

--- pages/blog/[slug].tsx
+++ pages/blog/[slug].tsx
@@ -1,20 +1,56 @@
-import getBlogPostContent, { BlogContent } from "../../utils/getBlogPostContent";
+import NextImage, { ImageProps } from "next/image";
+import getBlogPostContent, {
+  BlogContent,
+} from "../../utils/getBlogPostContent";
 
 import { ReactElement } from "react";
 import getBlogPosts from "../../utils/getBlogPosts";
-import { getMDXComponent } from 'mdx-bundler/client';
-import { useMemo } from 'react';
+import { getMDXComponent } from "mdx-bundler/client";
+import styled from "styled-components";
+import { useMemo } from "react";
 
 interface Props {
   content: BlogContent;
 }
+
+const Paragraph = styled.p`
+  color: ${(props): string => props.theme.color};
+`;
+
+const Title = styled.h1`
+  color: red;
+`;
+
+const SubTitle = styled.h2`
+  color: ${(props): string => props.theme.color};
+`;
+
+const Image = (props: ImageProps): ReactElement => {
+  return <NextImage {...props} />;
+};
+
+const BlogLayout = styled.div`
+  max-width: 1024px;
+  margin: 0 auto;
+`;
 
 export default function BlogPost({ content }: Props): ReactElement {
   const Component = useMemo(
     () => getMDXComponent(content.code),
     [content.code]
   );
-  return <div>Hello <Component /></div>;
+  return (
+    <BlogLayout>
+      <Component
+        components={{
+          p: Paragraph,
+          h1: Title,
+          h2: SubTitle,
+          Image: Image as any,
+        }}
+      />
+    </BlogLayout>
+  );
 }
 
 export async function getStaticProps({
@@ -34,7 +70,7 @@ export async function getStaticProps({
 // Statically generate all blog posts
 export async function getStaticPaths(): Promise<{
   paths: { params: { slug: string } }[];
-  fallback: boolean
+  fallback: boolean;
 }> {
   const blogPosts = getBlogPosts({});
 
